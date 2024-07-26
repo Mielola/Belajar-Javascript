bd7b781427b227ca9fb486305f03d07b1e204df7
Muhammad Wildan Dhiya Ulhaq <mwildab15@gmail.com>
2024-07-26 15:31:05 +0700

    Latihan Array

--- Array/scripts.js
+++ Array/scripts.js
@@ -5,11 +5,53 @@ const arrNamesAndClass = []
 arrNames.push("Wildan")
 arrNames.push("Wisnu", "Tatang")
 
-arrNamesAndClass.push({'nama' : 'Wildab', 'kelas' : '46 - 01'})
+arrNamesAndClass.push({'index' : 'Wildab', 'kelas' : '46 - 01'})
 console.table(arrNames)
 console.table(arrNamesAndClass)
 
 // delete array
 delete arrNames[1]
 console.table(arrNames)
+
+
+
+// Latihan
+const Mahasiswa =[]
+
+const addMahasiswa = (nama, kelas) => {
+    Mahasiswa.push({nama, kelas})
+}
+
+function displayMahasiswa() {
+    console.table(Mahasiswa)
+}
+
+const deleteMahasiswaWithIndex = (index) => {
+    delete Mahasiswa[index]
+}
+
+function deleteMahasiswaWithName(nama){
+    let index = Mahasiswa.findIndex(mahasiswa => mahasiswa.nama == nama)
+    if (index !== -1){
+        Mahasiswa.splice(index, 1)
+    }
+}
+
+// Menambahkan Mahasiswa
+addMahasiswa("Muhammad Wildab", "46 - 01")
+addMahasiswa("Muhammad Tatang", "46 - 03")
+addMahasiswa("Ranggaina", "46 - 04")
+
+console.log("Sebelum Dihapus")
+displayMahasiswa()
+
+// Menghapus Mahasiswa Menggunakan Index
+deleteMahasiswaWithIndex(1)
+console.log("Sesudah Dihapus Menggunakan index")
+displayMahasiswa()
+
+// Mengahpus Mahasiswa Menggunakan Nama
+deleteMahasiswaWithName("Muhammad Wildab")
+console.log("Sesudah Dihapus Menggunakan Nama")
+displayMahasiswa()
 
